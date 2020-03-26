c49aed5f83ec44df430392d701842d251fa45dee
Tom Durrant <tomchdurrant@gmail.com>
2020-03-26 21:46:22 +0000

    Completed login

---
+++ src/app/user/google-signin.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+
+@Directive({
+  selector: '[appGoogleSignin]'
+})
+export class GoogleSigninDirective {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  @HostListener('click')
+  onclick() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+}
