6773ba43fc8fbea6f69d656f3c202876ed56dd78
Tom Durrant <tomchdurrant@gmail.com>
2020-04-08 18:35:48 +0100

    Adding in board component and board list

---
+++ src/app/kanban/board/board.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { BoardService } from '../board.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Board } from '../board.model';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent {
+  @Input() board: Board;
+  constructor(private boardService: BoardService) { }
+
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
+}
